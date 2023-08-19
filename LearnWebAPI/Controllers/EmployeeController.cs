de8ceb57b27300484e7ee42a717e9d32407afcfa
Rahul Singh <rahulsdotnet@gmail.com>
2023-08-19 17:57:32 +0530

    Article 2 : Example for Custom Data Annontation Validation Attribute

---
+++ LearnWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody]Employee employee)
+        {
+            // Create Employee in DB
+            return Ok();
+        }
+
+        /// <summary>
+        /// Manual Data Annontation Validtion example, It can use in console app or other place
+        /// </summary>
+        /// <returns></returns>
+    public bool CreateEmployee()
+    {
+        var emp = new Employee()
+        {
+            Email = "test@example.com"
+        };
+
+        var validationContext = new ValidationContext(emp);
+        var validationResults = new List<ValidationResult>();
+
+        bool isValid = Validator.TryValidateObject(emp, validationContext, validationResults, true);
+
+        if (!isValid)
+        {
+            foreach (var result in validationResults)
+            {
+                Console.WriteLine(result.ErrorMessage);
+            }
+            return false;
+        }
+         // Create Employee in DB
+        return true;
+        }       
+    }
+
+    public class Employee
+    {
+        [Required]
+        [EmailAddress]
+        [DomainValidation("microsoft.com")]
+        public string Email { get; set; }
+    }
+}
---
+++ LearnWebAPI/Validations/DomainValidationAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearnWebAPI
+{
+    public class DomainValidationAttribute : ValidationAttribute
+    {
+        private readonly string _domain;
+        public DomainValidationAttribute(string domain)
+        {
+            _domain = domain;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is string email)
+            {
+                var splitEmail = email.Split("@");
+                return splitEmail.Length == 2 && splitEmail[1].Contains(_domain, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return $"{name} only allowed {_domain} domain.";
+        }
+    }
+}
